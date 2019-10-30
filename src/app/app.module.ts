a6b4d125f67bbd3e2252a9a3b99d9372e8300727
Makadown <maseflores79@gmail.com>
2019-10-30 15:40:59 -0600

    agregando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodosListComponent } from './todo/todos-list/todos-list.component';
+import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
+import { TodoAddComponent } from './todo/todo-add/todo-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    TodoComponent,
+    TodosListComponent,
+    TodoFooterComponent,
+    TodoAddComponent
   ],
   imports: [
     BrowserModule
