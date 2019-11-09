2a6b0471f4c4b2b3151edcc1bb3f8183dd225048
Makadown <maseflores79@gmail.com>
2019-11-09 07:56:46 -0600

    Agregando devtools module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { TodosListComponent } from './todo/todos-list/todos-list.component';
 import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
 import { TodoAddComponent } from './todo/todo-add/todo-add.component';
 import { TodoItemComponent } from './todo/todo-item/todo-item.component';
+// ngrx
+import { StoreModule } from '@ngrx/store';
+import { todoReducer } from './todo/todo.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -20,7 +25,12 @@ import { TodoItemComponent } from './todo/todo-item/todo-item.component';
     TodoItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot( { todos: todoReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }) 
   ],
   providers: [],
   bootstrap: [AppComponent]
